66a97389d38b1a096f16fbd43f1bf079af55da57
Ranjithvr48 <78946679+Ranjithvr48@users.noreply.github.com>
2023-06-11 21:19:48 +0530

    Add files via upload

---
+++ src/main/java/com/example/GeeksterAssign/GeeksterAssignApplication.java
@@ -0,0 +1,23 @@
+package com.example.GeeksterAssign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+@SpringBootApplication
+public class GeeksterAssignApplication {
+     @Autowired
+	 private EmailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksterAssignApplication.class, args);
+	}
+		@EventListener(ApplicationReadyEvent.class)
+	public void  sendMail(){
+		senderService.sendEmail("ranjithramalingam.rr@gmail.com",
+		 "This is the subject",
+			 "This is an junk mail");
+		}
+
+}
---
+++ src/test/java/com/example/GeeksterAssign/GeeksterAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GeeksterAssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksterAssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
